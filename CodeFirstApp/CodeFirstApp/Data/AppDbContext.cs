3e86a2f456ba3d2c14847d8b01abe38ec90992db
Mahammad V. Mustafayev <mahammadvm@code.local>
2023-06-02 11:27:25 +0400

    added

---
+++ CodeFirstApp/CodeFirstApp/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using CodeFirstApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApp.Data;
+
+internal class AppDbContext:DbContext
+{
+    public AppDbContext(){ }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<City> Cities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=Northwind; uid=sa;pwd=Alaska2017;TrustServerCertificate=True;");
+    }
+
+}
---
+++ CodeFirstApp/CodeFirstApp/Models/Country.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApp.Models;
+
+
+public abstract class Entity
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? PhoneCode { get; set; }
+}
+
+public class Country:Entity
+{
+    public Country()
+    {
+        this.Cities = new HashSet<City>();
+    }
+    public ICollection<City>? Cities { get; set; }
+}
+public class City : Entity
+{
+    //[ForeignKey("Country")]
+    //public string UlkeId { get; set; }
+    public int CountryId { get; set; }
+    public string? PlateCode { get; set; }
+    public Country? Country { get; set; }
+}
